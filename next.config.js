8e556a3db1644e7e82f149c2b09f1e03d810f335
AkramBENHAMMOU <141259806+AkramBENHAMMOU@users.noreply.github.com>
2025-04-01 14:17:39 +0000

    fix: configuration pour le déploiement Vercel

--- next.config.js
+++ next.config.js
@@ -5,10 +5,15 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+    dirs: ['src'],
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverActions: true,
+  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
